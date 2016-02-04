18b47f06ef2acead0245e64550e0f5eb9229ae62
Benoit Orihuela <borihuela@ozwillo.org>
2016-02-04 11:55:21 +0100

    [Evo] ref #207 - Port and integrate push to dashboard modal
    - Put common components in common-users.jsx.js
    - Remove now unused user-picker.jsx.js

--- portal-parent/ozwillo-portal-front/src/main/resources/public/js/appmanagement/instance-users.jsx.js
+++ portal-parent/ozwillo-portal-front/src/main/resources/public/js/appmanagement/instance-users.jsx.js
@@ -18,7 +18,6 @@ var ApplicationUsersManagement = React.createClass({
         $(this.getDOMNode()).modal('hide');
     },
     loadUsers: function() {
-        console.log("loading users");
         $.ajax({
             // only users that are app_user (so not those that are !app_user or app_admin)
             url: apps_service + "/users/instance/" + this.props.instanceId + "?app_admin=false",
@@ -30,6 +29,17 @@ var ApplicationUsersManagement = React.createClass({
             error: function (xhr, status, err) {
                 console.error(apps_service + "/users/instance/" + this.props.instanceId, status, err.toString());
                 this.setState({ users: []});
+            }.bind(this)
+        });
+    },
+    queryUsers: function(query, syncCallback, asyncCallback) {
+        $.ajax({
+            url: apps_service + "/users/network/" + this.props.authority + "?q=" + query,
+            dataType: 'json',
+            method: 'get',
+            success: asyncCallback,
+            error: function (xhr, status, err) {
+                console.error(apps_service + "/users/network/" + this.props.authority + "?q=" + query, status, err.toString());
             }.bind(this)
         });
     },
@@ -89,7 +99,7 @@ var ApplicationUsersManagement = React.createClass({
                             <h4 className="modal-title" id="modalLabel">{t('manage_users')}</h4>
                         </div>
                         <div className="modal-body">
-                            <OrgUserPicker addUser={this.addUser} authority={this.props.authority} />
+                            <OrgUserPicker addUser={this.addUser} queryUsers={this.queryUsers} />
                             <UsersEmailSelector addUsers={this.addUsersEmail} />
                             <UsersList users={this.state.users} removeUser={this.removeUser} />
                         </div>
@@ -106,101 +116,10 @@ var ApplicationUsersManagement = React.createClass({
     }
 });
 
-var UsersList = React.createClass({
-    /*
-    propTypes: {
-        users: React.PropTypes.array.required,
-        removeUser: React.PropTypes.func.required
-    },*/
-    render: function() {
-        var usersList = this.props.users.map(function(user) {
-            var userId = user.status === 'new_from_email' ? user.email : user.userid;
-            return <User key={userId} user={user} removeUser={this.props.removeUser(userId)} />;
-        }.bind(this));
-
-        return (
-            <div>
-                <table className="table table-striped table-responsive">
-                    <thead>
-                    <tr>
-                        <th>{t('name')}</th>
-                        <th>{t('status')}</th>
-                        <th>{t('actions')}</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                        {usersList}
-                    </tbody>
-                </table>
-            </div>
-        );
-    }
-});
-
-var User = React.createClass({
-    /*
-    propTypes: {
-        user: React.PropTypes.func.required,
-        removeUser: React.PropTypes.func.required
-    },*/
-    displayStatus: function(user) {
-        if (user.status === 'new_from_organization' || user.status === 'new_from_email')
-            return t('settings.status.new');
-        else
-            return t('settings.status.member');
-    },
-    render: function() {
-        return (
-            <tr>
-                <td>{this.props.user.fullname || this.props.user.email}</td>
-                <td>{this.displayStatus(this.props.user)}</td>
-                <td>
-                    <button className="btn oz-btn-danger" onClick={this.props.removeUser}>
-                        <i className="fa fa-trash"></i>
-                    </button>
-                </td>
-            </tr>
-        );
-    }
-});
-
-var OrgUserPicker = React.createClass({
-    getSelectedUsers: function() {
-        return this.state.users;
-    },
-    renderSuggestionTemplate: function(data) {
-        return '<div>' + data.fullname + '</div>';
-    },
-    queryUsers: function(query, syncCallback, asyncCallback) {
-        $.ajax({
-            url: apps_service + "/users/network/" + this.props.authority + "?q=" + query,
-            dataType: 'json',
-            method: 'get',
-            success: asyncCallback,
-            error: function (xhr, status, err) {
-                console.error(apps_service + "/users/network/" + this.props.authority + "?q=" + query, status, err.toString());
-            }.bind(this)
-        });
-    },
-    addUser: function(user, typeaheadRef) {
-        this.props.addUser(user);
-        typeaheadRef.typeahead('val', '');
-    },
-    render: function() {
-        return (
-            <div className="row">
-                <label htmlFor="search-user" className="control-label col-sm-2">{t('setting-add-from-organization')}</label>
-                <div className="col-sm-9">
-                    <Typeahead onSelect={this.addUser} source={this.queryUsers} placeholder={t('settings-add-a-user')}
-                               display="fullname" suggestionTemplate={this.renderSuggestionTemplate}
-                               fieldId="search-user" minLength={0} />
-                </div>
-            </div>
-        );
-    }
-});
-
 var UsersEmailSelector = React.createClass({
+    propTypes: {
+        addUsers: React.PropTypes.func.isRequired
+    },
     getInitialState: function() {
         return {
             hasError: false
@@ -209,6 +128,7 @@ var UsersEmailSelector = React.createClass({
     handleAddUsers: function() {
         var usersEmails = this.refs.usersEmail.getDOMNode().value.split(',');
         var areEmailsValid = usersEmails.every(function(email) {
+            // basic email validation, just want to spot typos, not more
             return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email.trim());
         })
         if (areEmailsValid) {
